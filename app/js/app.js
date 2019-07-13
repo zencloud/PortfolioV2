9e5b44e861ba14367b2f25204e9adfd147e96d8b
zencloud <mryanmill@gmail.com>
2019-07-13 09:09:37 -0600

    Letterbox BG Transition

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,47 @@
 new fullpage('#fullpage', {
+
+    anchors: ['stageWelcome', 'stageGamedev', 'stageAppdev', 'stageContact'],
     //sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-});
 
-let btnWelcome = document.getElementById("btn-welcome");
-btnWelcome.addEventListener('click', function() {
-
-    fullpage_api.moveSectionDown();
+    // Change letterbox color on transition
+    onLeave: function (origin, destination, direction) {
     
-    headerBG = document.getElementById("header-letterbox").style.backgroundColor = "#28814d";
 
+        // Welcome
+        if (destination.index === 0) {
+            document.getElementById("header-letterbox").style.backgroundColor = "#282828";
+        }
+
+        // Gamedev
+        if (destination.index === 1) {
+            document.getElementById("header-letterbox").style.backgroundColor = "#28814d";
+        }
+
+        // Appdev
+        if (destination.index === 2) {
+            document.getElementById("header-letterbox").style.backgroundColor = "#e77e22";
+        }
+
+        // Contact
+        if (destination.index === 1 && direction === 'down') {
+            document.getElementById("header-letterbox").style.backgroundColor = "#28814d";
+        }
+    },
 });
 
-// Scroll To Function
-function scrollTo(section) {
 
-    switch (section) {
-        case 1: 
-            fullpage_api.moveTo(section);
-            break;
-    }
-}
+// Navigation Listeners
+let btnWelcome = document.getElementById("btn-welcome");
+let btnGamedev = document.getElementById("btn-gamedev");
+
+
+// Add Listeners
+btnWelcome.addEventListener('click', function () {
+    fullpage_api.moveSectionDown();
+    headerBG = document.getElementById("header-letterbox").style.backgroundColor = "#28814d";
+});
+
+btnGamedev.addEventListener('click', function () {
+    fullpage_api.moveSectionDown();
+    headerBG = document.getElementById("header-letterbox").style.backgroundColor = "#28814d";
+});
