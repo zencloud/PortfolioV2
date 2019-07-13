ed6fee08e2086778fd27230de1208ad45d033562
zencloud <mryanmill@gmail.com>
2019-07-12 20:28:27 -0600

    Dev: Welcome

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,22 @@
 new fullpage('#fullpage', {
     //sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
 });
+
+let btnWelcome = document.getElementById("btn-welcome");
+btnWelcome.addEventListener('click', function() {
+
+    fullpage_api.moveSectionDown();
+    
+    headerBG = document.getElementById("header-letterbox").style.backgroundColor = "#28814d";
+
+});
+
+// Scroll To Function
+function scrollTo(section) {
+
+    switch (section) {
+        case 1: 
+            fullpage_api.moveTo(section);
+            break;
+    }
+}
